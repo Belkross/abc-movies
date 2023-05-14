59107684bf5a5cf3cc0989f127e714118235f64e
Belkross <belkross.pro@gmail.com>
2023-05-14 03:15:38 +0200

    feat: use the APP_NAME environment variable to have dynamic <h1></h1> and <title></title>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import { Stack, SxProps, Typography } from "@mui/material";
 
+const appName = import.meta.env.VITE_APP_NAME;
+
 export function Header() {
   return (
     <Stack component="header" sx={style_container}>
-      <Typography variant="h1">ABC Movies</Typography>
+      <Typography variant="h1">{appName}</Typography>
       <Typography>Create your own alphabetical movie selection</Typography>
     </Stack>
   );
