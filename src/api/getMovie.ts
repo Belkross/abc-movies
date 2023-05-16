44f0be5f08d7fe869966f905948218a22af5c1a1
Belkross <belkross.pro@gmail.com>
2023-05-16 23:16:56 +0200

    feat: parse the payload of the request "getMovie"

--- src/api/getMovie.ts
+++ src/api/getMovie.ts
@@ -1,8 +1,34 @@
+import Joi from "joi";
 import { OMDB_URL } from "../constants";
+
+const schema = Joi.object<GetMovieResponse>({
+  Response: Joi.string()
+    .regex(/^(True|False)$/)
+    .required(),
+  Error: Joi.string(),
+  Title: Joi.string().required(),
+  Year: Joi.string().pattern(/^\d{4}$/),
+  Actors: Joi.string(),
+  Plot: Joi.string(),
+  Poster: Joi.string(),
+  imdbID: Joi.string().required(),
+  Type: Joi.string()
+    .regex(/^movie$/)
+    .required(),
+}).unknown();
 
 export async function getMovie(id: string): Promise<GetMovieResponse> {
   const url = `${OMDB_URL}?apikey=${import.meta.env.VITE_OMDB_API_KEY}&i=${id}`;
-  const response = await fetch(url);
-  const payload = await response.json();
-  return payload as GetMovieResponse;
+
+  try {
+    const response = await fetch(url);
+    const payload = await response.json();
+    const { error, value } = schema.validate(payload);
+
+    if (error) throw new Error(error.message);
+    return value;
+  } catch (error) {
+    console.error(error);
+    throw new Error(error as string);
+  }
 }
