6225357972aaa79d77a007603073a03cf293ec76
Belkross <belkross.pro@gmail.com>
2023-05-16 23:37:30 +0200

    style: code presentation

--- src/api/getMovie.ts
+++ src/api/getMovie.ts
@@ -1,22 +1,6 @@
 import Joi from "joi";
 import { OMDB_URL } from "../constants";
 
-const schema = Joi.object<GetMovieResponse>({
-  Response: Joi.string()
-    .regex(/^(True|False)$/)
-    .required(),
-  Error: Joi.string(),
-  Title: Joi.string().required(),
-  Year: Joi.string().pattern(/^\d{4}$/),
-  Actors: Joi.string(),
-  Plot: Joi.string(),
-  Poster: Joi.string(),
-  imdbID: Joi.string().required(),
-  Type: Joi.string()
-    .regex(/^movie$/)
-    .required(),
-}).unknown();
-
 export async function getMovie(id: string): Promise<GetMovieResponse> {
   const url = `${OMDB_URL}?apikey=${import.meta.env.VITE_OMDB_API_KEY}&i=${id}`;
 
@@ -32,3 +16,19 @@ export async function getMovie(id: string): Promise<GetMovieResponse> {
     throw new Error(error as string);
   }
 }
+
+const schema = Joi.object<GetMovieResponse>({
+  Response: Joi.string()
+    .regex(/^(True|False)$/)
+    .required(),
+  Error: Joi.string(),
+  Title: Joi.string().required(),
+  Year: Joi.string().pattern(/^\d{4}$/),
+  Actors: Joi.string(),
+  Plot: Joi.string(),
+  Poster: Joi.string(),
+  imdbID: Joi.string().required(),
+  Type: Joi.string()
+    .regex(/^movie$/)
+    .required(),
+}).unknown();
