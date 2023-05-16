b5d55bf898ac785646835df75343f7a74684a2d1
Belkross <belkross.pro@gmail.com>
2023-05-16 23:43:19 +0200

    feat: parse the payload of the request "searchMovie"

--- src/api/searchMovies.ts
+++ src/api/searchMovies.ts
@@ -1,9 +1,39 @@
+import Joi from "joi";
 import { OMDB_URL } from "../constants";
 
 export async function searchMovies({ title, page }: { title: string; page: number }): Promise<SearchMoviesResponse> {
   const url = `${OMDB_URL}?type=movie&apikey=${import.meta.env.VITE_OMDB_API_KEY}&s=${title}&page=${page}`;
-  const response = await fetch(url);
-  const payload = await response.json();
 
-  return payload as SearchMoviesResponse;
+  try {
+    const response = await fetch(url);
+    const payload = await response.json();
+    const { error, value } = schema.validate(payload);
+
+    if (error) throw new Error(error.message);
+    return value;
+  } catch (error) {
+    console.error(error);
+    throw new Error(error as string);
+  }
 }
+
+const schema = Joi.object<SearchMoviesResponse>({
+  Response: Joi.string()
+    .regex(/^(True|False)$/)
+    .required(),
+  Error: Joi.string(),
+  Search: Joi.array().items(
+    Joi.object({
+      Title: Joi.string().required(),
+      Year: Joi.string()
+        .regex(/^\d{4}$/)
+        .required(),
+      imdbID: Joi.string().required(),
+      Type: Joi.string()
+        .regex(/^movie$/)
+        .required(),
+      Poster: Joi.string().required(),
+    })
+  ),
+  totalResults: Joi.string().regex(/^\d+$/),
+});
